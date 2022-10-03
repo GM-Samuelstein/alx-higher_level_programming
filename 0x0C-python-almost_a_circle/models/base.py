b73cfa125a510aa4c111f1cae7b2625004e640b9
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-10-03 10:44:56 -0700

    0x0C
    pycodestyle validation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,15 +58,15 @@ class Base:
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
-        l = []
+        load = []
         try:
             with open(filename, 'r') as f:
-                l = cls.from_json_string(f.read())
+                load = cls.from_json_string(f.read())
             for i, e in enumerate(l):
-                l[i] = cls.create(**l[i])
-        except:
+                load[i] = cls.create(**load[i])
+        except Exception:
             pass
-        return l
+        return load
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
@@ -86,7 +86,7 @@ class Base:
     def load_from_file_csv(cls):
         """deserializes a list of Rectangles/Squares in csv"""
         filename = cls.__name__ + ".csv"
-        l = []
+        load = []
         try:
             with open(filename, 'r') as csvfile:
                 csv_reader = csv.reader(csvfile)
@@ -102,9 +102,9 @@ class Base:
                                       "x": int(args[2]), "y": int(args[3])}
                     obj = cls.create(**dictionary)
                     l.append(obj)
-        except:
+        except Exception:
             pass
-        return l
+        return load
 
     @staticmethod
     def draw(list_rectangles, list_squares):
