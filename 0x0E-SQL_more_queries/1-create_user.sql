982a586f9027ef490718ff9d42924deffdde37ba
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-11-02 11:59:17 -0700

    All systems go

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- Creates the user user_0d_1 with all privileges.
+--- Script that creates the MYSQL server user user_0d_1.
+-- Creates the user user_0d_1 with all privilege.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
