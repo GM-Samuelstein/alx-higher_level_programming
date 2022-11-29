52d674eb239fcef92e815600e994c704342051e6
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-11-29 07:02:39 +0100

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-if (!process.argv[2]) {
+if (!process.argv[2] || isNaN(process.argv[2])) {
   console.log('Not a Number');
-} else if (Number(String(process.argv[2])) === 'NaN') {
-    console.log('Not a Number');
 } else {
   console.log('My number:', Math.trunc(Number(String(process.argv[2]))));
 }
