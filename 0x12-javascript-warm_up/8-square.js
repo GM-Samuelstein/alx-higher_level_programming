cddc36d62241f48d8f69835b2ad5e158cfaf784f
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-11-30 02:29:07 +0100

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,9 @@
 if (!process.argv[2] || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const s = [];
   for (let i = 1; i <= process.argv[2]; i++) {
     for (let j = 1; j <= process.argv[2]; j++) {
-      process.stdout.write('X')
+      process.stdout.write('X');
     }
     process.stdout.write('\n');
   }
