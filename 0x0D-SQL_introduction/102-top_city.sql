f0b7b636eb0cac97d19d6eb946b633ea1bc37415
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-12-02 20:20:20 +0100

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- temperatures between July and August.
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` = 7 OR `month` = 8
+WHERE `month` BETWEEN 7 AND 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
