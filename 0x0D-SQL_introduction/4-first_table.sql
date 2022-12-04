a7569838ada69d0071d311b043720bf525257d11
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-12-04 09:49:06 +0100

    4. First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,4 +4,4 @@ IF NOT EXISTS
     `first_table`(
         `id` INT DEFAULT NULL,
         `name` VARCHAR(256) DEFAULT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900-ai-ci;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
