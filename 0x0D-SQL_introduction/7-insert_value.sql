0002bfaf0868b60dc12a7d5a64a8cef40cc2ccd7
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-12-04 10:11:47 +0100

    7. First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,10 @@
+-- Inserts a new row into a table in a database in a MySQL server.
+INSERT INTO
+    `first_table` (
+        `id`,
+        `name`
+    )
+VALUES 
+    (89,
+    "Best School"
+    );
