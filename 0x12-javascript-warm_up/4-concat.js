eb47aea10b2374c275c14bb88e98fd5df3223de4
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-12-08 22:36:13 +0100

    4. Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-if (!process.argv[3]) {
+if (!process.argv[3] && !process.argv[2]) {
+  console.log('undefined is undefined');
+} else if (!process.argv[3]) {
   console.log(process.argv[2], 'is undefined');
 } else {
   console.log(process.argv[2], 'is', process.argv[3]);
