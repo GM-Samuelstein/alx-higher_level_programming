1194bf320923d8f74971627b771a59eb83a66d7d
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-12-09 01:05:09 +0100

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (!process.argv[2] || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i <= process.argv[2]; i++) {
+    let myStr = '';
+    for (let j = 1; j <= process.argv[2]; j++) {
+      myStr += 'X';
+    }
+    console.log(myStr);
+  }
+}
