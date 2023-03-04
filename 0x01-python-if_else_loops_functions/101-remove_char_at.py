9ae5ec7e3fa9d0311d2036c37622b97c0d7b0389
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-04 08:23:02 -0500

    15. Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,8 +5,10 @@ def remove_char_at(str, n):
     A python function that creates a copy of the string, removing the
     character at the position 'n'.
     """
+    nstring = ""
     for i in range(len(str)):
         if i == n:
             continue
         else:
-            print(f"{str[i]}", end="")
+            nstring = nstring + str[i]
+    return nstring
