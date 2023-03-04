3e119a9a0fc83c4e2cbcfe52c5e5d9b30f88139c
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-04 06:16:04 -0500

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,13 +2,14 @@
 
 def uppercase(str):
     """
-    A function that prints a string in uppercase.
+    A function that converts a string to uppercase.
     """
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             char = ord(str[i]) - 32
         else:
             char = ord(str[i])
+            
         print("{:c}".format(char), end="")
     
     print()
