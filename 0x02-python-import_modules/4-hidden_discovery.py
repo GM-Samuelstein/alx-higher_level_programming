34336c8da28857e7a14927c8278789ab615ddd68
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-11 06:35:34 -0500

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in dir(hidden_4):
-        if name[0] == "_" and name[1] == "_":
+    for definition in dir(hidden_4):
+        if definition[0] == "_" and definition[1] == "_":
             continue
         else:
-            print(name)
+            print(definition)
