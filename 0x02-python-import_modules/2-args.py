c6f575b5822eb570533461c6f2386524292871a3
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-11 04:16:58 -0500

    2. How to make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv) - 1
+    if n == 0:
+        print(f"{n:d} arguments.")
+    elif n == 1:
+        print(f"{n:d} argument:")
+    else:
+        print(f"{n:d} arguments:")
+
+if n != 0:
+    for i in range(len(sys.argv) - 1):
+        print(f"{i + 1}: {sys.argv[i + 1]}")
