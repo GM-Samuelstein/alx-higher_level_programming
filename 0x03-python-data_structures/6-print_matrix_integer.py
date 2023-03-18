1adcd355a4dd1230b0a39c457e0a186c3948eb64
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-18 04:56:40 -0400

    6. Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,5 @@ def print_matrix_integer(matrix=[[]]):
     """
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            if column != 0:
-                print(" ", end="")
-            print("{:d}".format(matrix[row][column]), end="")
-        print("")
+            print("{:d}".format(matrix[row][column]), end=" ")
+        print()
