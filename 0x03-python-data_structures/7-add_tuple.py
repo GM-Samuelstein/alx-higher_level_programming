d5cd59556ef8c566b64b587c07ee3de249a4d096
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-18 06:04:31 -0400

    7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
 
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    A function that adds 2 tuples.
+    """
+    new_tuple = ()
+
+    if len(tuple_a) == 0:
+        a = 0
+        b = 0
+    elif len(tuple_a) == 1:
+        a = tuple_a[0]
+        b = 0
+    else:
+        a, b = tuple_a
+
+    if len(tuple_b) == 0:
+        x = 0
+        y = 0
+    elif len(tuple_b) == 1:
+        x = tuple_b[0]
+        y = 0
+    else:
+        x, y = tuple_b
+
+    new_tuple = (a + x, b + y)
+    return new_tuple
