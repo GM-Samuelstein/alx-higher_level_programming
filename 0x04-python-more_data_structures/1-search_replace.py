02362d1efdfeba5b72ee87c190586996d080b0cd
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-19 12:39:27 -0400

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+    """
+    A function that replaces all occurrences of an element by another in a
+    new list.
+
+    Args:
+        my_list: The initial list.
+        search: The element to replace in the list.
+        replace: The new element.
+
+    Returns:
+        new_list: A new list that refelects the replacement(s).
+    """
+    new_list = my_list.copy()
+    for element in range(len(new_list)):
+        if new_list[element] == search:
+            new_list[element] = replace
+        else:
+            continue
+    return new_list
