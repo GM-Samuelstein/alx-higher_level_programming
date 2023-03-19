8fbf06a729457ab43fba773d507bb7ff121ce7bf
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-19 13:33:34 -0400

    4. Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+
 def only_diff_elements(set_1, set_2):
-    return (set_1 ^ set_2)
+    """
+    A function that returns a set of all elements present in only one set.
+
+    Args:
+        set_1: The first set.
+        set_2: The second set.
+
+    Returns:
+        od_set: A set containing the elements that are not present in the
+            two given sets.
+    """
+    od_set = set_1 ^ set_2
+
+    return od_set
