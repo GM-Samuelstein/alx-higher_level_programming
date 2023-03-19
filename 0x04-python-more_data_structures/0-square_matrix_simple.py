f51272d3d6eb4289e514930126b310cf80ce5266
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-19 12:24:23 -0400

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,13 @@
 def square_matrix_simple(matrix=[]):
     """
     A function that computes the square value of all integers of a matrix.
+
+    Args:
+        matrix: A 2 dimensional array.
+
+    Returns:
+        new_matrix: A new matrix of the same size as the input matrix,
+        and each value is the square of the value of the input.
     """
     new_matrix = []
     for row in matrix:
