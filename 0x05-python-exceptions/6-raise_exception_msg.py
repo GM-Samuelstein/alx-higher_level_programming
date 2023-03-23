c36d4096108866daf2821066a674f3bc27eaf2f9
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-23 18:31:34 -0400

    6. Raise a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
 def raise_exception_msg(message=""):
+    """
+    A function that raises a name exception with a message.
+
+    Args:
+        message: The message to be printed with the exception.
+
+    Returns:
+        No return value.
+    """
     raise NameError(message)
