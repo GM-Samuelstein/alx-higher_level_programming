fd2f8ee5792abb5856964d86bbb1a09c8a412075
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-22 22:04:29 -0400

    14. Squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    """
-    A function that computes the square value of all integers of a matrix
-    using `map`.
-
-    Args:
-        matrix: The given matrix, a 2 dimensional array.
-
-    Returns:
-        new_matrix: A new matrix of the same size as the input matrix,
-        and each value is the square of the value of the input.
-    """
-    new_matrix = matrix.copy()
-    # x = matrix row
-    # y = matrix column
-
-    new_matrix = \
-        list(map(lambda x: list(map(lambda y: y ** 2, x)), new_matrix))
-
-    return new_matrix
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
