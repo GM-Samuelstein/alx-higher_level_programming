9552c9e6b80e539344dda1bb91bda686b824f66f
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-23 18:15:30 -0400

    5. Raise exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
 def raise_exception():
+    """
+    A function that raises a type exception.
+
+    Args:
+        No args.
+
+    Returns:
+        No return value.
+    """
     raise TypeError
