07aaf8145cf8fdce4a0a25914092cdc310527fd3
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-23 20:56:58 -0400

    7. Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+
 def safe_print_integer_err(value):
+    """
+    
+    """
     import sys
     try:
         print("{:d}".format(value))
-    except Exception as i:
-        sys.stderr.write("Exception: {}\n".format(i))
-        return (False)
+    except ValueError as ve:
+        return False
+    except TypeError as te:
+        return False
     else:
-        return (True)
+        return True
