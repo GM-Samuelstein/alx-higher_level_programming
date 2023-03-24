a81ecbba1db48cc552c3b5852a24732f51fdff05
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-23 20:41:02 -0400

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    """
+    A function that prints x elements of a list.
+
+    Args:
+        my_list: The given list, which can contain any type.
+        x: The number of elements to be printed.
+
+    Returns:
+        nb_print: The real number of elements printed.
+
+    """
+    nb_print = 0
 
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except Exception:
-            break
-        else:
-            count += 1
-
-    print()
-    return (count)
+            print("{}".format(my_list[i]), end="")
+            nb_print += 1
+        except IndexError:
+            continue
+    print("")
+    return nb_print
