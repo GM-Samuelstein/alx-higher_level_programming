2c0aa7f98dd375b0fdbe644f1921bb2a613a34ef
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-25 19:07:50 -0400

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,90 @@
 #!/usr/bin/python3
-# 4-square.py
-"""Define a class Square."""
+"""
+Module 4-square.py.
+
+This module has only one class.
+
+Classes:
+    Square:
+        A class representing a Square.
+
+Functions:
+    This module has no functions.
+"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    A class representing a square.
+
+    Attributes:
+        size (int) : Private instance attribute.
+            The size of a new square object.
+
+    Methods:
+        __init__(self, size=0) :
+            Initializes a new square object with the given attribute.
+
+        size(self) : @property
+            Gets the property `size`.
+
+        size(self, value) : @size.setter
+            Sets the property `size`.
+
+        area(self) :
+            Returns the current square area.
+    """
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """
+        Initializes a new square object with the given attribute.
+
         Args:
-            size (int): The size of the new square.
+            size (int) : Private instance attribute.
+                The size of a new square object. The default value = 1.
+
         """
         self.size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """
+        This method gets the property `size`.
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        This method sets the property `size`.
+
+        Args:
+            size (int): Private instance attribute.
+                The size of a new square object.
+
+        Raises:
+            TypError:
+                If `size` is not an integer.
+            ValueError:
+                If `size` is less than zero.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """
+        Returns the current square area.
+
+        Args:
+            This method takes no arguments.
+
+        Returns:
+            square_area (int) :
+                The area of the square based on the value of `size `.
+        """
+        square_area = self.__size ** 2
+        return square_area
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -24,8 +24,8 @@ Here is a summary of what I learnt in this project:
 |[2-main.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/2-main.py)|A python file that tests the class: <code>Square:</code> defined in 2-square.py.|
 |[3-square.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/3-square.py)|A class that defines a square, and validates the type and value of attribute <code>size</code>, before initializing. The class also adds a method <code>def area(self):</code> that computes the area of the square.|
 |[3-main.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/3-main.py)|A python file that tests the class: <code>Square:</code> defined in 3-square.py.|
-|[4-square.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/4-square.py)||
-|[4-main.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/4-main.py)||
+|[4-square.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/4-square.py)|A class that defines a square, and initializes it with an attribute <code>size</code>. The class provides methods to access and update the private attribute of new objects. The class also adds a method <code>def area(self):</code> that computes the area of the square.|
+|[4-main.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/4-main.py)|A python file that tests the class: <code>Square:</code> defined in 4-square.py.|
 |[5-square.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/5-square.py)||
 |[5-main.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/5-main.py)||
 |[6-square.py](https://github.com/GM-Samuelstein/alx-higher_level_programming/blob/master/0x06-python-classes/6-square.py)||
