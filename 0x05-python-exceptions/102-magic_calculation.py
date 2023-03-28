782fa878c0171eb5c9b5d8f5d8b4f95c9fc7af71
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-03-28 18:46:56 +0100

    9. ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
+    """
+    A python function that does exactly the same as a given python bytecode.
+    """
     result = 0
+
     for i in range(1, 3):
         try:
-            if (i > a):
-                raise Exception("Too far")
+            if i > a:
+                raise Exception('Too far')
             else:
-                result += (a ** b) / i
+                result += a ** b / i
         except Exception:
             result = b + a
-            break
-    return (result)
+
+    return result
